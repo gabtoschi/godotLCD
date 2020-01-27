06e298d086d75e8be4069cb8d9dd55a22b4abb3d
Gabriel Toschi <gabriel.toschi@hotmail.com>
2020-01-26 22:17:47 -0300

    Implements children sprites in LCDSprite

--- addons/godotlcd/lcd_sprite.gd
+++ addons/godotlcd/lcd_sprite.gd
@@ -1,27 +1,56 @@
 tool
 extends Sprite
 
-const offSpriteAlpha = 0.2
+# type declaration
 enum LCDDisableType { ALPHA, DISABLE }
+var LCDSprite = get_script()
 
+# constants
+const offSpriteAlpha = 0.2
+
+# exported variables
+export var isHeadNode = false
 export(LCDDisableType) var offBehavior = LCDDisableType.ALPHA
 
+# non-exported variables
 var onColor: Color
 var offColor: Color
+var childNodes: Dictionary = {}
 
 func _enter_tree():
 	onColor = self.get_modulate()
 	offColor = self.get_modulate()
 	offColor.a = offSpriteAlpha
+	
+	if isHeadNode:
+		generateChildList()
+
+func generateChildList():
+	for node in self.get_children():
+		if node is LCDSprite:
+			childNodes[node.get_name()] = node
+			
+			if node.get_child_count() > 0:
+				node.generateChildList()
+				for childKey in node.childNodes.keys():
+					childNodes[childKey] = node.childNodes[childKey]
+
+func onNode(node):
+	if node is LCDSprite:
+		if offBehavior == LCDDisableType.DISABLE:
+			node.set_visible(true)
+		else:
+			node.set_modulate(onColor)
 
 func on():
-	if (offBehavior == LCDDisableType.DISABLE):
-		self.set_visible(true)
-	else:
-		self.set_modulate(onColor)
+	onNode(self)
+
+func offNode(node):
+	if node is LCDSprite:
+		if offBehavior == LCDDisableType.DISABLE:
+			node.set_visible(false)
+		else:
+			node.set_modulate(offColor)
 
 func off():
-	if (offBehavior == LCDDisableType.DISABLE):
-		self.set_visible(false)
-	else:
-		self.set_modulate(offColor)
+	offNode(self)
